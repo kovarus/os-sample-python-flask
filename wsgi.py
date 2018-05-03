1cb87098fb520527a50379ef9eec090d9848e1d7
Leon Levy <leon.s.levy@gmail.com>
2018-05-03 16:02:03 -0700

    added date

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 
 import socket
+import datetime
+
 
 application = Flask(__name__)
 
@@ -14,9 +16,13 @@ def hello():
 @application.route('/')
 @application.route('/index')
 def index():
+    now = datetime.datetime.now()
     user = {'username': 'Leon'}
     hostname = socket.gethostname()
-    return render_template('index.html', title='Flask on OpenShift', user=user, hostname=hostname)
+    return render_template('index.html', title='Flask on OpenShift',
+                           user=user,
+                           hostname=hostname,
+                           time=now.strftime('%m-%d-%Y %H:%M'))
 
 
 if __name__ == '__main__':
